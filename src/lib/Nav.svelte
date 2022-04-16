fa60f1baeff8788bfabeb579a6b3c388d7e2c3b3
phptuts <glaserpower@gmail.com>
2022-04-15 22:04:56 -0700

    first commit

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,9 @@
+<nav>
+	<h1>Nav Bar</h1>
+</nav>
+
+<style>
+	nav {
+		background-color: red;
+	}
+</style>
---
+++ src/lib/Teacher.svelte
@@ -0,0 +1,4 @@
+<section>
+	<img src="https://via.placeholder.com/300/09f.png/fff" alt="teacher pic" />
+	<section>Teacher</section>
+</section>
