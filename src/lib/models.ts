8d32c9449fee31b34447861b4469686f53ec0548
phptuts <glaserpower@gmail.com>
2022-04-19 22:44:32 -0700

    new model updates

--- src/lib/models.ts
+++ src/lib/models.ts
@@ -1,20 +1,40 @@
 import type { serverTimestamp } from 'firebase/firestore';
-
-// This contains a list of text the teacher has share during her class.
-export interface Text<T> {
-	text: string;
-	version: number;
-	created_at: T;
+// https://firebase.google.com/docs/firestore/manage-data/add-data#custom_objects
+// Implement Custom Objects
+// Don't do sub collections because we don't need to query on them
+// Keep things simple just having a bunch of sessions
+enum ContentType {
+	HTML,
+	CSS,
+	JS,
+	Text
 }
 
-// A sessions like a class
-export interface Session<T> {
+// This contains a list of text the teacher has share during her class.
+export interface ContentBlock {
+	type: ContentType;
+	text: string;
+}
+
+export interface Page {
+	title: string;
+	textblocks: [number: ContentBlock];
+}
+
+// This turns into a class
+export interface Lesson<T> {
 	id: string;
 	is_active: boolean;
 	nickname: string;
 	created_at: T;
+	teacher_id: string;
+	pageVersions: [number: Page];
 }
 
+// Idea this takes up the right side of the page in website
+// that's expandable
+
+// This turns into a class
 // This represents a teacher
 export interface Teacher<T> {
 	id: string;
